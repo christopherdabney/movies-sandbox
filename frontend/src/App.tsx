9f60974a4ab11ab8b5da0ac252d98140fbd36590
christopherdabney <christopher.dabney@yahoo.com>
2025-09-26 13:25:58 -0500

    new commit, protected routes for react router.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import Register from './pages/Register.tsx'
 import Login from './pages/Login.tsx'
 import Home from './pages/Home.tsx'
@@ -16,7 +17,11 @@ function App() {
           <Route path="/" element={<div>Root page</div>} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
-          <Route path="home" element={<Home />} />
+          <Route path="home" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } />
         </Routes>
       </main>
     </Router>
---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import { accountRecord } from '../store/accountSlice'
+import type { AppDispatch, RootState } from '../store/store'
+
+interface ProtectedRouteProps {
+  children: React.ReactNode
+}
+
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const dispatch = useDispatch<AppDispatch>()
+  const { account, loading } = useSelector((state: RootState) => state.account)
+  const [authChecked, setAuthChecked] = useState(false)
+
+  useEffect(() => {
+    if (!account && !authChecked && !loading) {
+      dispatch(accountRecord())
+        .finally(() => setAuthChecked(true))
+    } else if (account && !authChecked) {
+      setAuthChecked(true)
+    }
+  }, [dispatch, account, authChecked, loading]) // Add loading to dependencies
+
+  // Still checking auth
+  if (!authChecked && loading) {
+    return <div>Loading...</div>
+  }
+
+  // Auth check complete, but no account - redirect
+  if (authChecked && !account) {
+    return <Navigate to="/login" replace />
+  }
+
+  // User is authenticated or still loading initial check
+  if (account) {
+    return <>{children}</>
+  }
+
+  return <div>Loading...</div>
+}
+
+export default ProtectedRoute
